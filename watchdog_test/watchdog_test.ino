8a2e6a6bb4888b3a9ecdbc4313c6ce970bb66e7d
Chris Ramsay <chris@ramsay-family.net>
2014-10-22 21:00:36 +0100

    Watchdog testing - part 2

--- watchdog_test/watchdog_test.ino
+++ watchdog_test/watchdog_test.ino
@@ -10,35 +10,35 @@ void setup() {
   Serial.println("Arduino reset");
   pinMode(led, OUTPUT); 
   // Send an initial heartbeat.
-  heartbeat();
+  heartbeat("Initial heartbeat");
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // Check for serial inputs.  If found, send heartbeat.
-  if (Serial.available()) {
+  //if (Serial.available()) {
     // Clear input buffer
-    while (Serial.available()) {
-      Serial.read();
-    }
-    heartbeat();
+    //while (Serial.available()) {
+    //  Serial.read();
+    //}
+    heartbeat("Heartbeat sent");
     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 	delay(1000);               // wait for a second
 	digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-	delay(1000);  
-  }
+	delay(1000); 
+  //}
   unsigned long uptime = millis();
-  if ((uptime - lastUptimeReport) >= 5000) {
+  if ((uptime - lastUptimeReport) >= 10000) {
     // It has been at least 5 seconds since our last uptime report.  
-    Serial.println("Uptime: " + String((uptime - (uptime % 5000)) / 1000) + " seconds (" + String((uptime - lastHeartbeat) / 1000) + " seconds since last heartbeat)");
+    Serial.println("Uptime: " + String((uptime - (uptime % 10000)) / 1000) + " seconds (" + String((uptime - lastHeartbeat) / 1000) + " seconds since last heartbeat)");
     // Pretend we did it exactly on the 5 second mark so we don't start slipping.
-    lastUptimeReport = (uptime - (uptime % 5000));
+    lastUptimeReport = (uptime - (uptime % 10000));
   }
   // delay in between loops
   delay(100);
 }
 
-void heartbeat() {
+void heartbeat(String message) {
   // Sink current to drain charge from watchdog circuit
   pinMode(pulsePin, OUTPUT);
   digitalWrite(pulsePin, LOW);
@@ -46,5 +46,5 @@ void heartbeat() {
   // Return to high-Z
   pinMode(pulsePin, INPUT);
   lastHeartbeat = millis();
-  Serial.println("Heartbeat sent");
+  Serial.println(message);
 }
